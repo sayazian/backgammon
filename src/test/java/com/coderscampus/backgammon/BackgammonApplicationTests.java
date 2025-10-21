934daec127cbdc8ab66948adbb48057afacf0bcb
sayazian <sahar.ayazian@gmail.com>
2025-10-21 16:10:14 -0700

    Initial commit

---
+++ src/test/java/com/coderscampus/backgammon/BackgammonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.backgammon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgammonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
