193745d8758768fafb33dc93bd155e0f80c605ee
sayazian <sahar.ayazian@gmail.com>
2025-11-07 09:21:24 -0800

    initial commit

---
+++ src/main/java/com/example/backgammon_vanilla/BackgammonVanillaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backgammon_vanilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgammonVanillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackgammonVanillaApplication.class, args);
+	}
+
+}
